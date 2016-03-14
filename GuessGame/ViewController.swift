dbd0c0effc5709302a223bd3bf12a48835f8be0d
raul <raultc@macbook-pro-de-r.local>
2016-03-14 17:59:38 -0600

    Initial Commit

---
+++ GuessGame/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GuessGame
+//
+//  Created by Raúl T on 14/03/16.
+//  Copyright © 2016 rtc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
