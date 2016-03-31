db13016b042f9cf27974d3c0ef0159fee771181b
raul <raultc@MacBook-Pro-de-R.local>
2016-03-30 19:01:18 -0600

    juego aha

---
+++ GuessGame/InfoViewController.swift
@@ -0,0 +1,30 @@
+//
+//  InfoViewController.swift
+//  GuessGame
+//
+//  Created by Raúl T on 28/03/16.
+//  Copyright © 2016 rtc. All rights reserved.
+//
+
+import UIKit
+
+
+class InfoViewController: UIViewController {
+    
+    @IBAction func close(sender: AnyObject) {
+    
+       dismissViewControllerAnimated(true, completion: nil)
+        
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        
+    }
+    
+}
--- GuessGame/ViewController.swift
+++ GuessGame/ViewController.swift
@@ -10,9 +10,119 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var randomNumber: UILabel!
+    
+    @IBOutlet weak var slider: UISlider!
+    @IBOutlet weak var round: UILabel!
+    @IBOutlet weak var scoreLabel: UILabel!
+
+    var targetValue = 0
+    var roundNumber = 1
+    var scoreCounter = 0
+    var points = 0
+    var difference = 0
+    
+    
+    @IBAction func hitMe(sender: AnyObject) {
+        
+        getScore()
+       // let currentValue = Int(slider.value)
+
+       // sliderValue.text = "\(currentValue)"
+        //generateRandomNumber()
+        slider.value = 50
+        
+        //scoreLabel.text = String(scoreCounter)
+        
+        showMessage()
+        
+        
+        
+    }
+    
+    func showMessage() {
+        
+        
+        
+        var title = ""
+        
+        if difference == 0 {
+            title = "perfect"
+        }
+        else if difference < 5 {
+            title = "muy cerca"
+        }
+        else if difference < 10 {
+            title = "casi"
+        }
+        else {
+            title = "nada bien"
+        }
+        
+        // crea el mensaje de alerta
+        let alert = UIAlertController(title: title, message: "You scored \(points) points", preferredStyle: UIAlertControllerStyle.Alert)
+        
+        //crea el boton para la alerta
+        let action = UIAlertAction(title: "Ok", style: .Default, handler: { action in
+            
+            self.generateRandomNumber()
+            self.slider.value = 50
+            self.scoreLabel.text = String(self.scoreCounter)
+            self.roundNumber += 1
+            self.round.text = "\(self.roundNumber)"
+
+        })
+        
+        //añade la accion para que aparezca el boton
+        alert.addAction(action)
+        
+        self.presentViewController(alert, animated: true, completion: nil)
+        
+    }
+    
+    
+    
+    func getScore(){
+        
+        difference = abs(Int(slider.value) - targetValue)
+        //scoreCounter = (100 - difference)
+       // print(difference)
+        points = (100 - difference)
+        scoreCounter += points
+        
+    }
+    
+    @IBAction func starOver(sender: AnyObject) {
+        
+        roundNumber = 1
+        scoreCounter = 0
+        round.text = "\(roundNumber)"
+
+        generateRandomNumber()
+        slider.value = 50
+        
+        scoreLabel.text = String(scoreCounter)
+        
+    }
+    
+    
+    func generateRandomNumber()  {
+        
+        targetValue = 1 + Int(arc4random_uniform(100))
+        randomNumber.text = String(targetValue)
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        generateRandomNumber()
+        scoreCounter = 0
+        slider.value = 50
+        scoreLabel.text = String(scoreCounter)
+        round.text = "1"
+        
     }
 
     override func didReceiveMemoryWarning() {
